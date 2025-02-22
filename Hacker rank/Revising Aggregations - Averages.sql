d1b26295f3fec72464bcecdd6833934336ed3d6a
HazemMedhat <102475810+HazemMedhat@users.noreply.github.com>
2025-02-23 01:24:35 +0200

    Create Revising Aggregations - Averages.sql

---
+++ Hacker rank/Revising Aggregations - Averages.sql
@@ -0,0 +1,5 @@
+% Problem Link : https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+
+SELECT avg(POPULATION ) 
+FROM CITY
+WHERE DISTRICT = 'California';
