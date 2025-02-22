de1e9ec6c4fff6977834c82e6b8f370264b8150b
HazemMedhat <102475810+HazemMedhat@users.noreply.github.com>
2025-02-22 23:36:40 +0200

    Delete Revising the Select Query II.sql

--- Revising the Select Query II.sql
+++
@@ -1,5 +0,0 @@
-% Problem Link : https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
-
-select NAME
-from CITY
-where POPULATION > 120000 and COUNTRYCODE = 'USA';
