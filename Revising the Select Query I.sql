91cd2036b1e9d3d7f1ce686c8654d9d562b2a49f
HazemMedhat <102475810+HazemMedhat@users.noreply.github.com>
2025-02-22 23:02:17 +0200

    Create Revising the Select Query I.sql

---
+++ Revising the Select Query I.sql
@@ -0,0 +1,5 @@
+% Problem Link : https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+  
+select * 
+from CITY
+where POPULATION > 100000 and COUNTRYCODE = 'USA';
